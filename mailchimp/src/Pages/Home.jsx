13a9285b6d1544d771ca2b0261c65d9de45b389c
YelveTejas <tejas.yelve20@gmail.com>
2022-11-12 05:28:47 +0530

    day4

--- mailchimp/src/Pages/Home.jsx
+++ mailchimp/src/Pages/Home.jsx
@@ -1,8 +1,33 @@
-import { Box, Button,  Container, Divider, Heading, HStack, Image, Select, Stat, StatHelpText, StatLabel, StatNumber, Text, VStack } from '@chakra-ui/react'
-import { CheckIcon } from '@chakra-ui/icons'
-import React from 'react'
+import { Box, Button,  Container, Divider, Grid, GridItem, Heading, Highlight, HStack, Image, Select, SimpleGrid, Stat, StatHelpText, StatLabel, StatNumber, Text, VStack } from '@chakra-ui/react'
+import { CheckIcon,ChevronDownIcon, ChevronRightIcon, RepeatClockIcon } from '@chakra-ui/icons'
+import '../styles/shadow.css'
+
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuItemOption,
+  MenuGroup,
+  MenuOptionGroup,
+  MenuDivider,
+} from '@chakra-ui/react'
+
+import React, { useState } from 'react'
+import { useEffect } from 'react'
+import axios from 'axios'
 
 const Home = () => {
+const [data,setData] = useState([])
+
+useEffect(()=>{
+  axios.get('http://localhost:3004/posts')
+  .then((res)=>{
+    setData(res.data)
+    console.log(res.data)
+  })
+},[])
+
   return (
     <div>
     <Container marginTop='2cm' >
@@ -15,8 +40,8 @@ const Home = () => {
 
             <Button borderRadius='20px' mt='1cm' bg='yellow' borderColor='black' variant='solid'>Sing Up</Button>
     </Container>
-  <Box  w='full' display='flex' margin='auto' mt='30px' borderColor='black' height='19cm'>
-    <Box w='25%' h='14cm'>
+  <Box w='full' display='flex' margin='auto' mt='30px' borderColor='black' height='19cm'>
+    <Box className='shadow' w='25%' h='14cm'>
       <Image src='https://eep.io/images/yzco4xsimv0y/33f3mgIWbzKjIpIRJi2f5v/6bd36bba38b7f6a94d587605ab9a7d75/Desktop_Feature_Cards_1_Automations.jpeg?w=630&fm=webp&q=80' alt=''></Image>
       <Box w='full' h='4cm' backgroundColor='yellow'>
       <Text pr='10px' pl='10px' pt='1cm' fontSize='md' align='left'>Send the right message at the right time to convert more customers.</Text>
@@ -58,7 +83,7 @@ const Home = () => {
   </Box>
    <Text fontSize='30px' mt='1cm' >Find the Right Plan for Me</Text>
       <HStack w='80%' margin='auto' h='13cm' spacing={20}>
-        <VStack w='20%' align='left'>
+        <VStack w='20%' align='left' className='shadow'>
           <Text fontWeight='bold' fontSize='25px'>Premium</Text>
           <Text align='left'>Our most advanced tools, unlimited contacts, and priority support; built for teams.</Text>
           <Stat align='left'>
@@ -87,7 +112,7 @@ const Home = () => {
           </HStack>
           <Divider/>
         </VStack>
-        <VStack w='20%' align='left'>
+        <VStack w='20%' align='left' className='shadow'>
         <Text fontWeight='bold' fontSize='25px'>Standard</Text>
           <Text align='left'>Our most advanced tools, unlimited contacts, and priority support; built for teams.</Text>
           <Stat align='left'>
@@ -116,7 +141,7 @@ const Home = () => {
           </HStack>
           <Divider/>
         </VStack>
-        <VStack w='20%'>
+        <VStack w='20%' align='left' className='shadow'>
         <Text fontWeight='bold' fontSize='25px'>Essential</Text>
           <Text align='left'>Our most advanced tools, unlimited contacts, and priority support; built for teams.</Text>
           <Stat align='left'>
@@ -145,7 +170,7 @@ const Home = () => {
           </HStack>
           <Divider/>
         </VStack>
-        <VStack w='20%'>
+        <VStack w='20%' align='left' className='shadow'>
         <Text fontWeight='bold' fontSize='25px'>Free</Text>
           <Text align='left'>Our most advanced tools, unlimited contacts, and priority support; built for teams.</Text>
           <Stat align='left'>
@@ -179,13 +204,147 @@ const Home = () => {
         <Box backgroundColor='yellow.400' h='full' w='50%' >
           <VStack w='80%' margin='auto' marginTop='4cm'>
             <Text fontWeight='normal' color='black' fontSize='4xl'>Keep customers ready to buy with engaging content</Text>
-           
+            <Menu>
+            <MenuButton as={Button} bg='yellow.400' rightIcon={<ChevronDownIcon />}>
+              Content Optimizer
+            </MenuButton>
+            <MenuList bg='yellow.400' borderColor='yellow.400'>
+              <MenuItem w='7cm' bg='yellow.400'>
+              <Text>Learn how your emails compare to the top-performing campaigns in your industry and get data-driven suggestions for improving your copy, imagery, and layout.</Text>
+                </MenuItem>
+              
+            </MenuList>
+           </Menu>
+           <Divider color='grey'/>
+           <Menu>
+            <MenuButton as={Button} bg='yellow.400' rightIcon={<ChevronDownIcon />}>
+              Creative Assistant
+            </MenuButton>
+            <MenuList bg='yellow.400'>
+              <MenuItem w='7cm'>
+              <Text>Learn how your emails compare to the top-performing campaigns in your industry and get data-driven suggestions for improving your copy, imagery, and layout.</Text>
+                </MenuItem>
+
+                <MenuItem>
+                <Button bg='yellow.400'>
+                  Learn More <ChevronRightIcon/>
+                  </Button>
+                </MenuItem>
+              
+            </MenuList>
+           </Menu>
           </VStack>
         </Box>
-        <Box></Box>
+        <Box>
+          <VStack w='15cm'  margin='auto' mt='2cm'>
+        <Heading fontWeight='semibold'>Discover new ways to automate</Heading>
+        <Text  fontSize='2xl' >Get a head start with pre-built journeys that help you cross-sell your products, recover abandoned carts, re-engage existing customers, and win new ones.</Text>
+        
+         <Text mt='1cm' fontSize='2xl'>Learn More <ChevronRightIcon/></Text>
+       
+          </VStack>
+
+          <VStack w='15cm' margin='auto' mt='2cm' >
+        <Heading fontWeight='medium' fontSize='3xl'>Keep your emails relevant and your brand growing</Heading>
+        <Text  fontSize='2xl' >Deliver personalized emails based on their buying behavior, survey responses, chat interactions, and support tickets to promote loyalty and growth.</Text>
+        
+         <Text mt='1cm' fontSize='2xl'>Learn More <ChevronRightIcon/></Text>
+       
+          </VStack>
+        </Box>
        </Box>
 
+       <Container maxW='lg' mt='2cm'>
+        <Text fontSize='30px' align='center'>Get up to 88% more revenue* with our Customer Lifetime Value and Likelihood to Purchase segments</Text>
+       </Container>
+       <Box w='12cm' marginRight='4cm'>
+        <Text fontSize='30px'>Bring in more data, drive more growth with our integrations</Text>
+       </Box>
+       <Grid
+      templateColumns={{lg:'repeat(3,1fr)',md:'repeat(2,1fr)',sm:'repeat(1,1fr)'}}
+      h='10cm'
+      width='80%'
+      margin='auto'
+      gap={5}
+      mt='2cm'
+       >
+        {
+          data.map((e)=>(
+            <GridItem key={e.id}>
+                  <HStack >
+                    <Image h='2.5cm' w='2.5cm'  src={e.img} alt={e.title}></Image>
+                    <VStack>
+                      <Text align='left' fontSize='lg' fontWeight='medium'>{e.title}</Text>
+                      <Text fontSize='sm'>{e.des}</Text>
+                    </VStack>
+                  </HStack>
 
+                 </GridItem>
+          ))
+        }
+
+      </Grid>
+      <Box w='full'display='flex'  h='15cm' backgroundColor='#E7B75F' mt='4cm' justifyContent='space-around'>
+       
+          <VStack mt='2cm'>
+            <Text fontSize='2xl' fontWeight='medium'>Product</Text>
+            <Text>Why</Text>
+            <Text>MaiilChimp</Text>
+            <Text>Product</Text>
+            <Text>Updates</Text>
+            <Text>Email</Text>
+            <Text>Marketing</Text>
+          </VStack>
+          <VStack mt='2cm'>
+            <Text fontSize='2xl' fontWeight='medium'>Resources</Text>
+            <Text>Marketing</Text>
+            <Text>Free Marketing</Text>
+            <Text>Tools</Text>
+            <Text>Marketing</Text>
+            <Text>Glossary</Text>
+            <Text>Inetegration</Text>
+            <Text>Directory</Text>
+          </VStack>
+          <VStack mt='2cm'>
+            <Text fontSize='2xl' fontWeight='medium'>Community</Text>
+            <Text>Agencies & Freelancers</Text>
+            <Text>Developers</Text>
+            <Text>Events</Text>
+           
+          </VStack>
+          <VStack mt='2cm'>
+            <Text fontSize='2xl' fontWeight='medium'>Company</Text>
+            <Text>Our Story</Text>
+            <Text>News Rooms</Text>
+            <Text>Annuals</Text>
+            <Text>Report</Text>
+            <Text>Careers</Text>
+            <Text>Accessibility</Text>
+         
+          </VStack>
+          <VStack mt='2cm'>
+            <Text fontSize='2xl' fontWeight='medium'>Help</Text>
+            <Text>Contact Us</Text>
+            <Text>Hire an </Text>
+            <Text>Expert</Text>
+            <Text>Help</Text>
+            <Text>Talk to</Text>
+            <Text>Sales</Text>
+          </VStack>
+          <VStack mt='2cm'>
+            <Image w='5cm' src='https://eep.io/images/yzco4xsimv0y/73ZAoe8O3Y7SXQLeCNTq5u/8ddf1f63b4abdf7f67149d3a9d74a0bd/MCP.svg?w=260&fm=webp&q=80' alt=''></Image>
+            <Text w='4cm' fontSize='sm'> Films, podcasts, and original series that celebrate the entrepreneurial spirit.</Text>
+            <Text >Check it out <ChevronRightIcon/></Text>
+            <Divider></Divider>
+            <Image mt='10px' w='4cm' src='https://eep.io/images/yzco4xsimv0y/5itQmdXHB6vMuAcvyWp5MR/ef5cd2cecd4c92b7029a22ec67585c65/Courier.svg?w=260&fm=webp&q=80' alt=''></Image>
+            <Text w='4cm' fontSize='sm'>Expert insights, industry trends, and inspiring stories that help you live and work on your own terms.</Text>
+            <Text>Learn More <ChevronRightIcon/></Text>
+          </VStack>
+        
+      </Box>
+      
+
+       
 
     </div>
 
